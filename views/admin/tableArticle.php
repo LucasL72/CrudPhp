b3140bbbd8dbcb830d65bb8624e0718132d20fdc
LucasL72 <lucasj.ledoux@gmail.com>
2022-03-07 15:26:21 +0100

    crud ok

--- views/admin/tableArticle.php
+++ views/admin/tableArticle.php
@@ -40,55 +40,57 @@
     </div>
 
     <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
-        <div class="card-body">
-            <table class="table">
-                <thead class="thead-dark">
-                    <tr class="text-center">
-                        <th scope="col">#</th>
-                        <th scope="col">title</th>
-                        <th scope="col">subtitle</th>
-                        <th scope="col">description</th>
-                        <th scope="col">Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
+        <!-- PUT Cards -->
+        <div class="container mb-5">
+            <h2>Method PUT</h2>
+            <div class="row">
 
-                    <?php
-                    // Appel de la class Database et de ça fontion connect()
-                    $pdo = Database::connect();
-                    $sql = 'SELECT * FROM articles ORDER BY id';
-                    $q = $pdo->query($sql);
-
-                    foreach ($q as $row) {
-
-                    ?>
-
-                        <tr class="text-center">
-                            <th scope="row"> <?php echo $row['id'] ?> </th>
-                            <td><?php echo $row['title'] ?></td>
-                            <td><?php echo $row['subtitle'] ?></td>
-                            <td><?php echo $row['description'] ?></td>
-                            <td>
-                                <button data-target='#modalEditArticle' class="btn btn-warning btn-sm" data-toggle="modal" data-id="<?php echo $row['id'] ?>" data-title="<?php echo $row['title'] ?>" data-subtitle="<?php echo $row['subtitle'] ?>" data-description="<?php echo $row['description'] ?>">
-                                    <i class="far fa-edit"></i>
-                                </button>
-                                <button data-target='#modalDeleteArticle' class="btn btn-danger btn-sm" data-toggle="modal" data-id="<?php echo $row['id'] ?>" data-title="<?php echo $row['title'] ?>">
-                                    <i class="far fa-trash-alt"></i>
-                                </button>
-                            </td>
-                        </tr>
+                <!-- Connexion à la DB -->
+                <?php
+                // import de la connexion Connexion à la DB
+                include_once 'db/database.php';
+                // Appel de la class Database et de ça fontion connect()
+                $pdo = Database::connect();
 
 
+                $sql = 'SELECT * FROM articles';
+                $q = $pdo->query($sql);
 
-                    <?php };
+                foreach ($q as $row) {
 
 
-                    // Appel de la class Database et de ça fontion disconnect()
-                    Database::disconnect();
-
-                    ?>
-                </tbody>
-            </table>
+                ?>
+                    <div class="col">
+                        <div class="card" style="width: 18rem;">
+                            <img src="./public/images/creative1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <form action="./controller/update.php?id=<?php echo $row['id'] ?>" method="POST">
+                                    <h5><?php echo $row['id'] ?></h5>
+                                    <h5>
+                                        <label for="articlenameedit" class="form-label">Edit name</label>
+                                        <input type="text" class="form-control" placeholder="<?php echo $row['title'] ?>" name="title">
+                                    </h5>
+                                    <h5>
+                                        <label for="contentedit" class="form-label">Edit subtitle</label>
+                                        <input type="text" class="form-control" placeholder="<?php echo $row['subtitle'] ?>" name="subtitle">
+                                    </h5>
+                                    <h5>
+                                        <label for="contentedit" class="form-label">Edit description</label>
+                                        <input type="text" class="form-control" placeholder="<?php echo $row['description'] ?>" name="description">
+                                        <button type="submit" class="btn btn-success mt-3">Edit</button>
+                                    </h5>
+                                </form>
+                                <form action="./controller/delete.php" method="POST">
+                                    <input type="hidden" name="id" value="<?php echo $row['id'] ?>">
+                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                <?php };
+                Database::disconnect();
+                ?>
+            </div>
         </div>
 
     </div>
